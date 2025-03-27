ddbbf7ea70557ca26f8dace2c8f715517560b12c
Jedi1aaa79 <eddy.cui@outlook.com>
2025-03-27 13:11:56 +0200

    finished hero section image bluur

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function scrollToPackages() {
+    document.getElementById("packages").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToDemos() {
+    document.getElementById("demo").scrollIntoView({ behavior: "smooth" });
+}
+
+
+function scrollToContact() {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+}
